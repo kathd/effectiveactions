c5c868be3e31334cd49fe48f09d7aa3abc3075ff
Kathleen Domingo <kathleensdomingo@gmail.com>
2020-04-05 23:19:32 +0200

    added login formm

--- frontend/src/components/forms/LoginForm.jsx
+++ frontend/src/components/forms/LoginForm.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Button, Checkbox, Form } from "semantic-ui-react";
 
-const LoginForm = () => {
-  return (
-    <div>
-      Log In Form here
-    </div>
-  )
-}
+const LoginForm = () => (
+  <Form>
+    <Form.Field>
+      <label>Email</label>
+      <input placeholder="Email" type="email" />
+    </Form.Field>
+    <Form.Field>
+      <label>Password</label>
+      <input placeholder="Password" type="password" />
+    </Form.Field>
+    <Form.Field>
+      <Checkbox label="I agree to the Terms and Conditions" />
+    </Form.Field>
+    <Button type="submit">Submit</Button>
+  </Form>
+);
 
-export default LoginForm
+export default LoginForm;
