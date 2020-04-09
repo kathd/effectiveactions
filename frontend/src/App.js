dec91fef1a64541b058f10b142e87fdd6817492a
Kathleen Domingo <kathleensdomingo@gmail.com>
2020-04-09 10:03:34 +0200

    added mission views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import "./styles/index.css";
 import Navbar from "./components/Navbar";
 import Login from "./views/Login";
 import Home from "./views/Home";
+import OurMission from "./views/OurMission";
 import Articles from "./views/Articles";
 import Actions from "./views/Actions";
 import NotFound from "./views/NotFound";
@@ -17,6 +18,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/articles" component={Articles} />
+        <Route path="/our-mission" component={OurMission} />
         <Route path="/login" component={Login} />
         <Route path="/actions/:type" component={Actions} />
         <Route path="*" component={NotFound} />
---
+++ frontend/src/views/OurMission.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const OurMission = () => {
+    return (
+        <div>
+        <h1>Our Mission</h1>
+            {/* mission statement here */}
+        </div>
+    )
+}
+
+export default OurMission
